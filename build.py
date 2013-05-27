1a4b4c1013ef996a4b0e427eca4e52c799bdacb0
Matt Lebrun <lebrun.matt@gmail.com>
2013-05-27 19:16:59 +0800

    Added a build file.

---
+++ build.py
@@ -0,0 +1,28 @@
+from subprocess import call
+
+# Configurations
+settings = {
+    'input_dir': '.content',
+    'output_dir': '.',
+    'settings': 'settings.py',
+    'purge_list': [
+        'feeds',
+        'category',
+        'author',
+        'tag',
+        'theme',
+        '*.html',
+    ],
+}
+
+com = {
+    'build': 'pelican {} -o {} -s {}'.format(settings['input_dir'], settings['output_dir'], settings['settings']),
+    'delete': 'rm -Rf {}',
+}
+
+# Start Build
+for i in settings['purge_list']:
+    call(com['delete'].format(i), shell=True) 
+
+call(com['build'], shell=True)
+
