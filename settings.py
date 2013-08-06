c16e86c4af81a8d533080ce83faf7052668a3c6e
Matt Lebrun <lebrun.matt@gmail.com>
2013-08-06 17:43:26 +0800

    New build and settings update

--- settings.py
+++ settings.py
@@ -9,21 +9,21 @@ DEFAULT_DATE_FORMAT = '%A, %B %d, %Y'
 SUMMARY_MAX_LENGTH = (50)
 
 GITHUB_URL = 'https://github.com/cr8ivecodesmith/cr8ivecodesmith.github.com'
-# DISQUS_SITENAME = 'cr8ivecodesmith'
+DISQUS_SITENAME = 'cr8ivecodesmith'
 GOOGLE_ANALYTICS = 'UA-9679489-4'
 
 FEED_DOMAIN = SITEURL
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 
-SOCIAL = (('twitter', 'https://twitter.com/cr8ivecodesmith'),
-          ('github', 'https://github.com/cr8ivecodesmith'),
-          ('google+', 'https://plus.google.com/113492352729403053751'),)
+SOCIAL = (('@cr8ivecodesmith', 'https://twitter.com/cr8ivecodesmith'),
+          ('opensource', 'https://github.com/cr8ivecodesmith'),
+          ('email', 'mailto:lebrun.matt@gmail.com'),)
 TWITTER_USERNAME = 'cr8ivecodesmith'
 
 THEME = '.pelican_themes/pelican-svbhack'
 TIMEZONE = 'Asia/Manila'
-DEFAULT_PAGINATION = 1
+DEFAULT_PAGINATION = 3
 
 MARKUP = (('md', 'rst'))
 MD_EXTENSIONS = (['codehilite', 'extra'])
