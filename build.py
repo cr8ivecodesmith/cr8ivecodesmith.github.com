e94ce509cbae9d1faf5d32aead9fa1a884f5d20e
Matt Lebrun <lebrun.matt@gmail.com>
2013-08-06 16:35:05 +0800

    Update settings and theme

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from subprocess import call
 
 # Configurations
 settings = {
-    'input_dir': '.content',
+    'input_dir': 'content',
     'output_dir': '.',
     'settings': 'settings.py',
     'purge_list': [
@@ -12,12 +12,14 @@ settings = {
         'tag',
         'theme',
         '*.html',
+	'*.pyc',
     ],
 }
 
 com = {
     'build': 'pelican {} -o {} -s {}'.format(settings['input_dir'], settings['output_dir'], settings['settings']),
     'delete': 'rm -Rf {}',
+    'delete_index': 'ls | egrep "index[0-9]+\.html" | xargs rm',
 }
 
 # Start Build
@@ -25,3 +27,6 @@ for i in settings['purge_list']:
     call(com['delete'].format(i), shell=True)
 
 call(com['build'], shell=True)
+
+call(com['delete_index'].format(i), shell=True)
+
--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'matt lebrun'
 SITENAME = 'programmers are people too'
+TAGLINE = 'Deviant Programmer/Creative, Pythonista, Lover of Opensource, in pursuit of Awsomer.'
 SITEURL = 'http://mattlebrun.com'
 DEFAULT_DATE_FORMAT = '%A, %B %d, %Y'
 SUMMARY_MAX_LENGTH = (50)
@@ -14,12 +15,12 @@ FEED_DOMAIN = SITEURL
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 
-# SOCIAL = (('twitter', 'https://twitter.com/cr8ivecodesmith'),
-#           ('github', 'https://github.com/cr8ivecodesmith'),
-#           ('google+', 'https://plus.google.com/113492352729403053751'),)
-# TWITTER_USERNAME = 'cr8ivecodesmith'
+SOCIAL = (('twitter', 'https://twitter.com/cr8ivecodesmith'),
+          ('github', 'https://github.com/cr8ivecodesmith'),
+          ('google+', 'https://plus.google.com/113492352729403053751'),)
+TWITTER_USERNAME = 'cr8ivecodesmith'
 
-THEME = 'mnmlist'
+THEME = '.pelican_themes/pelican-svbhack'
 TIMEZONE = 'Asia/Manila'
 DEFAULT_PAGINATION = 1
 
