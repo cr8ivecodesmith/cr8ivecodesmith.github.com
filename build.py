8ae34abf87e37479a107ef0053be6a800d41d541
Matt Lebrun <lebrun.matt@gmail.com>
2013-12-16 09:44:22 +0800

    Added to gitignore and purge list

--- build.py
+++ build.py
@@ -13,13 +13,13 @@ settings = {
         'theme',
         '*.html',
         '*.pyc',
+        '__pycache__',
     ],
 }
 
 com = {
     'build': 'pelican {} -o {} -s {}'.format(settings['input_dir'], settings['output_dir'], settings['settings']),
     'delete': 'rm -Rf {}',
-    'delete_index': 'ls | egrep "index[0-9]+\.html" | xargs rm',
 }
 
 # Start Build
@@ -27,5 +27,3 @@ for i in settings['purge_list']:
     call(com['delete'].format(i), shell=True)
 
 call(com['build'], shell=True)
-
-# call(com['delete_index'].format(i), shell=True)
